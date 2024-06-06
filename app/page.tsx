ce314efa5b2da4626142a949f64a7bcfaa30d06b
zheqi <2481550454@qq.com>
2024-06-06 13:27:50 +0800

    feat: 优化图片

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
-import Image from 'next/image';
+import Image from "next/image";
+const base64 =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAACCAIAAADwyuo0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAIklEQVR4nGNgYFRcMnfV/y9/GbkVGdjUnDNzW1JyOxhYlAF40Ah0YMgUXQAAAABJRU5ErkJggg==";
 
-export default async function Home() {  
+export default async function Home() {
   return (
     <main>
       <Image
         src="/bg.png"
         alt="background"
         placeholder="blur"
-        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAACCAIAAADwyuo0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAIklEQVR4nGNgYFRcMnfV/y9/GbkVGdjUnDNzW1JyOxhYlAF40Ah0YMgUXQAAAABJRU5ErkJggg=="
+        blurDataURL={base64}
         quality={90}
-        sizes='100vw 100vh'
-        fill={true}
+        sizes="(max-width: 768px) 768px, (max-width: 1200px) 1200px, (max-width: 1920px) 1920px, 3840px"
+        fill
         style={{
-          objectFit: 'cover',
+          objectFit: "cover",
         }}
       />
     </main>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  experimental: {},
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
